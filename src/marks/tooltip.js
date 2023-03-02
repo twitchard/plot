6214727ffacffede4d4161e92a980c17d14c0ec6
Mike Bostock <mbostock@gmail.com>
2023-03-02 08:28:33 -0800

    maxRadius; pointerdown to suppress

--- src/marks/tooltip.js
+++ src/marks/tooltip.js
@@ -1,10 +1,10 @@
-import {least, pointer, select} from "d3";
+import {pointer, select} from "d3";
 import {Mark} from "../mark.js";
 import {maybeFrameAnchor, maybeTuple} from "../options.js";
 
 export class Tooltip extends Mark {
   constructor(data, options = {}) {
-    const {x, y, frameAnchor} = options;
+    const {x, y, maxRadius = 40, frameAnchor} = options;
     super(
       data,
       {
@@ -14,37 +14,58 @@ export class Tooltip extends Mark {
       options
     );
     this.frameAnchor = maybeFrameAnchor(frameAnchor);
-    this.indexBySvg = new WeakMap();
+    this.indexesBySvg = new WeakMap();
+    this.maxRadius = +maxRadius;
   }
   render(index, scales, {x: X, y: Y, fx: FX, fy: FY}, dimensions, context) {
     // TODO
-    // - ✅ Use d3.pointer to get local coordinates?
-    // - ✅ Look up the closest point in screen (scaled) coordinates?
-    // - ✅ Determine the closest point across facets?
+    // - ✅ Get local coordinates of the pointer
+    // - ✅ Register one pointermove listener per plot
+    // - ✅ Find the closest point in screen (scaled) coordinates
+    // - ✅ Find the closest point across all facets
+    // - ✅ Limit the search radius
+    // - ✅ Suppress the tooltip on pointerdown
     // - Handle multiple dots in the same position (e.g., click to cycle)?
-    // - Maybe invert the screen coordinates to abstract coordinates? (probably no)
-    // - Only register one pointermove listener per plot (rather than per facet)?
     // - Handle x or y not existing; respect frameAnchor
     // - Handle fx or fy not existing (for the entire plot)
     // - Handle faceting being disabled for this mark (facet: null)
+    // - Display the unscaled values in a tooltip
+    // - Remove the red dot for testing purposes
+    const {maxRadius} = this;
     const {marginLeft, marginTop} = dimensions;
     const svg = context.ownerSVGElement;
-    let flatIndex = this.indexBySvg.get(svg);
-    if (flatIndex) return void flatIndex.push(...index); // TODO faster?
-    flatIndex = Array.from(index); // TODO faster?
-    this.indexBySvg.set(svg, flatIndex);
+    let indexes = this.indexesBySvg.get(svg);
+    if (indexes) return void indexes.push(index);
+    this.indexesBySvg.set(svg, (indexes = [index]));
     const dot = select(svg)
       .on("pointermove", (event) => {
-        const [x, y] = pointer(event);
-        const i = least(flatIndex, (i) => Math.hypot(FX[i] + X[i] - marginLeft - x, FY[i] + Y[i] - y - marginTop));
-        dot
-          .attr("r", 4.5)
-          .attr("cx", FX[i] + X[i] - marginLeft)
-          .attr("cy", FY[i] + Y[i] - marginTop);
+        let i, xi, yi;
+        if (event.buttons === 0) {
+          const [xp, yp] = pointer(event);
+          let ri = maxRadius * maxRadius;
+          for (const index of indexes) {
+            for (const j of index) {
+              const xj = FX[j] + X[j] - marginLeft;
+              const yj = FY[j] + Y[j] - marginTop;
+              const dx = xj - xp;
+              const dy = yj - yp;
+              const rj = dx * dx + dy * dy;
+              if (rj <= ri) (i = j), (ri = rj), (xi = xj), (yi = yj);
+            }
+          }
+        }
+        if (i === undefined) {
+          dot.attr("display", "none");
+        } else {
+          dot.attr("display", "inline");
+          dot.attr("cx", xi);
+          dot.attr("cy", yi);
+        }
       })
-      .on("pointerleave", () => dot.attr("r", 0))
+      .on("pointerdown pointerleave", () => dot.attr("display", "none"))
       .append("circle")
-      .attr("r", 0)
+      .attr("display", "none")
+      .attr("r", 4.5)
       .attr("stroke", "red")
       .attr("fill", "none")
       .attr("stroke-width", 1.5);
