803e501af0314119edddad0e8cbbd34ddb82b7d9
Mike Bostock <mbostock@gmail.com>
2023-03-02 08:32:38 -0800

    prettier

--- test/plots/athletes-tooltip.js
+++ test/plots/athletes-tooltip.js
@@ -3,11 +3,12 @@ import * as d3 from "d3";
 
 export async function athletesTooltip() {
   const athletes = await d3.csv("data/athletes.csv", d3.autoType);
+  const decade = (d) => `${Math.floor(d.date_of_birth.getUTCFullYear() / 10) * 10}`;
   return Plot.plot({
     grid: true,
     marks: [
-      Plot.dot(athletes, {x: "weight", y: "height", fx: "sex", fy: (d) => Math.floor(d.date_of_birth.getUTCFullYear() / 10)}),
-      Plot.tooltip(athletes, {x: "weight", y: "height", fx: "sex", fy: (d) => Math.floor(d.date_of_birth.getUTCFullYear() / 10)})
+      Plot.dot(athletes, {x: "weight", y: "height", fx: "sex", fy: decade}),
+      Plot.tooltip(athletes, {x: "weight", y: "height", fx: "sex", fy: decade})
     ]
   });
 }
