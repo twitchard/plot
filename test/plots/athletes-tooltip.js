224fe6076eb9d25e1e8d153da5e55fa488442c57
Mike Bostock <mbostock@gmail.com>
2023-03-02 09:06:29 -0800

    use fy transform

--- test/plots/athletes-tooltip.js
+++ test/plots/athletes-tooltip.js
@@ -3,12 +3,15 @@ import * as d3 from "d3";
 
 export async function athletesTooltip() {
   const athletes = await d3.csv("data/athletes.csv", d3.autoType);
-  const decade = (d) => `${Math.floor(d.date_of_birth.getUTCFullYear() / 10) * 10}`;
   return Plot.plot({
     grid: true,
+    fy: {
+      label: "decade of birth",
+      transform: (d) => `${Math.floor(d.getUTCFullYear() / 10) * 10}`
+    },
     marks: [
-      Plot.dot(athletes, {x: "weight", y: "height", fx: "sex", fy: decade}),
-      Plot.tooltip(athletes, {x: "weight", y: "height", fx: "sex", fy: decade})
+      Plot.dot(athletes, {x: "weight", y: "height", fx: "sex", fy: "date_of_birth"}),
+      Plot.tooltip(athletes, {x: "weight", y: "height", fx: "sex", fy: "date_of_birth"})
     ]
   });
 }
